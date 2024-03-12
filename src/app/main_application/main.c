7d06a4629a25dc8632a79f054c0c8b2f289b1c03
CompileMyLife <ibcinematografy@gmail.com>
2024-03-11 22:57:19 -0700

    added menu task and other tasks to be setup later

--- src/app/main_application/main.c
+++ src/app/main_application/main.c
@@ -15,51 +15,70 @@
 #include "microblaze_sleep.h"
 #include "xil_printf.h"
 
-// Alias for UART Lite Peripheral
-#define UART0_BASEADDR XPAR_AXI_UARTLITE_0_BASEADDR
-#define UART1_BASEADDR XPAR_AXI_UARTLITE_1_BASEADDR
+// Alias for Interrupt Controller Peripheral
+#define INTC_DEVICE_ID          XPAR_MICROBLAZE_0_AXI_INTC_DEVICE_ID
 
+// Alias for USB UART Lite Peripheral
+#define USB_UART_BASEADDR 		XPAR_AXI_UARTLITE_0_BASEADDR
+#define AXI_USB_UART_INTR_NUM   XPAR_MICROBLAZE_0_AXI_INTC_AXI_UARTLITE_0_INTERRUPT_INTR
 
-void vMonitorUART1Task(void *pvParameters) {
-    (void)pvParameters; // Unuseds parameter
-    char ch, c;
-    char test1 = 'a';
-    char test2 = 'b';
-    char newline = '\n';
-    xil_printf("Monitoring\r\n");
-    for (;;) {
+// Alias for PMOD UART Lite Peripheral
+#define PMOD_UART_BASEADDR 		XPAR_AXI_UARTLITE_1_BASEADDR
+#define AXI_PMOD_UART_INTR_NUM  XPAR_MICROBLAZE_0_AXI_INTC_AXI_UARTLITE_1_INTERRUPT_INTR
 
-    	XUartLite_SendByte(UART1_BASEADDR, test1);
-    	//xil_printf("Sending UART1: %c\r\n", test1);
+// Alias for AXI Timer Peripheral
+#define AXI_TIMER_INTR_NUM      XPAR_MICROBLAZE_0_AXI_INTC_AXI_TIMER_0_INTERRUPT_INTR
 
-    	XUartLite_SendByte(UART1_BASEADDR, newline);
-    	//xil_printf("Sending UART1: %c\r\n", newline);
+int init_sys(void);
 
-    	XUartLite_SendByte(UART0_BASEADDR, test2);
-    	//xil_printf("Sending UART0: %c\r\n", test1);
+void vMenuTask(void *pvParameters);
 
-    	XUartLite_SendByte(UART0_BASEADDR, newline);
-    	//xil_printf("Sending UART0: %c\r\n", newline);
+void vDataTask(void *pvParameters);
 
-    	if (XUartLite_IsReceiveEmpty(UART1_BASEADDR) == FALSE) {
-    	   ch = XUartLite_RecvByte(UART1_BASEADDR);
-    	   xil_printf("UART1 Received: %c\r\n", ch);
-    	}
-    	if (XUartLite_IsReceiveEmpty(UART0_BASEADDR) == FALSE) {
-    	   c = XUartLite_RecvByte(UART0_BASEADDR);
-    	   xil_printf("UART0 Received: %c\r\n", c);
-    	}
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
+void vWarmUpTask(void *pvParameters);
 
+void vWorkSetTask(void *pvParameters);
+
+typedef struct {
+	uint8_t curr_lift_type;
+	uint8_t curr_warmup_mode;
+	uint8_t prev_lift_type;
+	uint8_t prev_warmup_mode;
+	bool    warmup_done;
+} UserInput, *UserInputPtr;
+
+TaskHandle_t xMenu    = NULL;
+TaskHandle_t xData    = NULL;
+TaskHandle_t xWarmUp  = NULL;
+TaskHandle_t xWorkSet = NULL;
+
+XIntc        irq;
+
+UserInputPtr user_input;
 
 int main(void) {
     // Initialize platform
     init_platform();
-    xil_printf("Program started. Awaiting UART0 input...\r\n");
 
-    xTaskCreate(vMonitorUART1Task, "Monitor UART1 Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+    if (init_sys() != XST_SUCCESS) {
+    	cleanup_platform();
+    	return -1;
+    }
+
+    print("BarBuddy Initializing...\r\n");
+
+    xTaskCreate(vMenuTask, "MENU TASK", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &xMenu);
+    xTaskCreate(vDataTask, "DATA TASK", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &xData);
+    xTaskCreate(vWarmUpTask, "WARMUP TASK", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &xWarmUp);
+    xTaskCreate(vWorkSetTask,"WORKSET TASK", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &xWorkSet);
+
+
+    vTaskSuspend(xData);
+    vTaskSuspend(xWarmUp);
+    vTaskSuspend(xWorkSet);
+
+
+    vTaskResume(xMenu);
 
     // Start the scheduler
     vTaskStartScheduler();
@@ -67,4 +86,112 @@ int main(void) {
     // Should never reach here
     cleanup_platform();
     return 0;
+}
+
+int init_sys(void) {
+	int status;
+
+	// Initialize the Interrupt Controller
+	status = XIntc_Initialize(&irq, INTC_DEVICE_ID);
+	if (status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	// Start the Interrupt Controller such that interrupts are enabled for all
+	// devices that cause interrupts
+	status = XIntc_Start(&irq, XIN_REAL_MODE);
+	if (status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	// Enable the AXI Timer and UART lite interrupts
+	XIntc_Enable(&irq, AXI_TIMER_INTR_NUM);
+	XIntc_Enable(&irq, AXI_USB_UART_INTR_NUM);
+	XIntc_Enable(&irq, AXI_PMOD_UART_INTR_NUM);
+
+	return XST_SUCCESS;
+}
+
+void vMenuTask(void *pvParameters) {
+	print("DEBUG: Entered vMenuTask()\r\n");
+
+	for (;;){
+		print("INFO:vMenuTask()\tECE 544 Final Project\r\n");
+		print("INFO:vMenuTask()\tBy Ibrahim Binmahfood (ibrah5@pdx.edu)\r\n");
+		print("INFO:vMenuTask()\tand Robert Wilcox     (wilcox6@pdx.edu)\r\n");
+
+		// Prompt User to select a lift out of list
+		print("Select a Lift from the list by the number it is associated with: \r\n");
+		print("0 [Squat], 1 [Bench], 2 [Bicep Curl]...\r\n");
+		user_input->curr_lift_type = XUartLite_RecvByte(USB_UART_BASEADDR);
+
+		if (isdigit(user_input->curr_lift_type)) {
+			// Check if the previous lift type was called again
+			if (user_input->prev_lift_type == user_input->curr_lift_type) {
+				xil_printf("DEBUG:Lift Type: %d\tWarmUp Set Done: %s\r\n",
+						user_input->prev_lift_type,
+						user_input->prev_warmup_done ? "true" : "false");
+
+				vTaskResume(xWorkSet);
+
+				vTaskSuspend(xWarmUp);
+				vTaskSuspend(xData);
+
+				vTaskSuspend(NULL); // Suspend vMenuTask
+			}
+
+			// Prompt User to select Warm Up MODE for that lift
+			print("Select WarmUp MODE [y/n]: \r\n");
+			user_input->curr_warmup_mode = XUartLite_RecvByte(USB_UART_BASEADDR);
+
+			if (user_input->curr_warmup_mode == 'y') {
+				// Set current values to previous values
+			    user_input->prev_lift_type   = user_input->curr_lift_type;
+				user_input->prev_warmup_mode = user_input->curr_warmup_mode;
+
+				vTaskResume(xWarmUp);
+
+				vTaskSuspend(xData);
+				vTaskSuspend(xWorkSet);
+
+				vTaskSuspend(NULL);	// Suspend vMenuTask
+			}
+
+			else {
+
+				print("Require to Select 'y' for WarmUp MODE to record a Lift's WarmUp Set\r\n");
+			}
+		}
+
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
+
+void vDataTask(void *pvParameters) {
+	print("DEBUG:Entered vDataTask()\r\n");
+
+	for (;;){
+
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
+
+void vWarmUpTask(void *pvParameters) {
+	print("DEBUG:Entered vWarmUpTask()\r\n");
+
+	for (;;){
+
+		user_input->warmup_done = true;
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
+
+void vWorkSetTask(void *pvParameters) {
+	print("DEBUG:Entered vWorkSetTask()\r\n");
+
+	for (;;){
+
+
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
 }
